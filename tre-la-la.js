bbb9266dd252376446ed9fd7b7b6f0776e2ab313
Scott Schroeder <scotts@navinet.net>
2014-03-18 13:04:27 -0400

    Add comment accessing JQuery from Confluence.

--- tre-la-la.js
+++ tre-la-la.js
@@ -1,4 +1,6 @@
 // This is Tre-la-la for Trello.
+
+// Leverage Confluence AJS.$ to access JQuery
 AJS.$(document).ready(function() {
 	var obj = document.getElementById('tre-la-la');
 	obj.innerHTML = "I'm here";
