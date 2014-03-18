51f05448a759a5dbe18a6d4cc9c138203e2bda3d
Scott Schroeder <scotts@navinet.net>
2014-03-18 13:56:34 -0400

    try a chart

--- tre-la-la.js
+++ tre-la-la.js
@@ -2,6 +2,25 @@
 
 // Leverage Confluence AJS.$ to access JQuery
 AJS.$(document).ready(function() {
-	var obj = document.getElementById('tre-la-la');
-	obj.innerHTML = "I'm here";
+	google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('tre-la-la'));
+        chart.draw(data, options);
+      }
+	//var obj = document.getElementById('tre-la-la');
+	//obj.innerHTML = "I'm here";
 });
