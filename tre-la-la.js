3d69e257284dbdc8a7b9645c5d70f1b0a7828e1d
Scott Schroeder <scotts@navinet.net>
2014-03-18 14:20:02 -0400

    fix loading.

--- tre-la-la.js
+++ tre-la-la.js
@@ -1,26 +1,26 @@
 // This is Tre-la-la for Trello.
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+	function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+	  ['Year', 'Sales', 'Expenses'],
+	  ['2004',  1000,      400],
+	  ['2005',  1170,      460],
+	  ['2006',  660,       1120],
+	  ['2007',  1030,      540]
+	]);
+
+	var options = {
+	  title: 'Company Performance',
+	  vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
+	};
+
+	var chart = new google.visualization.BarChart(document.getElementById('tre-la-la'));
+	chart.draw(data, options);
+}
 
 // Leverage Confluence AJS.$ to access JQuery
 AJS.$(document).ready(function() {
-	google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
-
-        var options = {
-          title: 'Company Performance',
-          vAxis: {title: 'Year',  titleTextStyle: {color: 'red'}}
-        };
-
-        var chart = new google.visualization.BarChart(document.getElementById('tre-la-la'));
-        chart.draw(data, options);
-      }
 	//var obj = document.getElementById('tre-la-la');
 	//obj.innerHTML = "I'm here";
 });
