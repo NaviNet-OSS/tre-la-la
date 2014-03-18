1426005ce430bbe6c6ce3264d8899b9363256b0f
Scott Schroeder <scotts@navinet.net>
2014-03-18 17:11:07 -0400

    Refactor CFD chart creation

--- tre-la-la.js
+++ tre-la-la.js
@@ -48,84 +48,69 @@ function createPercentageCompleteChart(id, complete) {
     });
 }
 
+function createCfdChart(id) {
+	$(id).highcharts({
+		chart: {
+			type: 'area'
+		},
+		title: {
+			text: 'Historic and Estimated Worldwide Population Growth by Region'
+		},
+		subtitle: {
+			text: 'Source: Wikipedia.org'
+		},
+		xAxis: {
+			categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+			tickmarkPlacement: 'on',
+			title: {
+				enabled: false
+			}
+		},
+		yAxis: {
+			title: {
+				text: 'Billions'
+			},
+			labels: {
+				formatter: function() {
+					return this.value / 1000;
+				}
+			}
+		},
+		tooltip: {
+			shared: true,
+			valueSuffix: ' millions'
+		},
+		plotOptions: {
+			area: {
+				stacking: 'normal',
+				lineColor: '#666666',
+				lineWidth: 1,
+				marker: {
+					lineWidth: 1,
+					lineColor: '#666666'
+				}
+			}
+		},
+		series: [{
+			name: 'Asia',
+			data: [502, 635, 809, 947, 1402, 3634, 5268]
+		}, {
+			name: 'Africa',
+			data: [106, 107, 111, 133, 221, 767, 1766]
+		}, {
+			name: 'Europe',
+			data: [163, 203, 276, 408, 547, 729, 628]
+		}, {
+			name: 'America',
+			data: [18, 31, 54, 156, 339, 818, 1201]
+		}, {
+			name: 'Oceania',
+			data: [2, 2, 2, 6, 13, 30, 46]
+		}]
+	});
+}
+
 AJS.$(document).ready(function() {
-	//var obj = document.getElementById('tre-la-la');
-	//obj.innerHTML = "I'm here";
-	
-	createPercentageCompleteChart('tre-la-la-percent-complete-highcharts', 85)
-	
-	var data = [
-		{
-			value: 30,
-			color:"#F7464A"
-		},
-		{
-			value : 70,
-			color : "#00CC66"
-		},
-	]
-	
-	//Get the context of the canvas element we want to select
-	var ctx = document.getElementById("tre-la-la-percent-complete").getContext("2d");
-	var myNewChart = new Chart(ctx).Doughnut(data);
-	
-	        $('#tre-la-la-cfd').highcharts({
-            chart: {
-                type: 'area'
-            },
-            title: {
-                text: 'Historic and Estimated Worldwide Population Growth by Region'
-            },
-            subtitle: {
-                text: 'Source: Wikipedia.org'
-            },
-            xAxis: {
-                categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-                tickmarkPlacement: 'on',
-                title: {
-                    enabled: false
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Billions'
-                },
-                labels: {
-                    formatter: function() {
-                        return this.value / 1000;
-                    }
-                }
-            },
-            tooltip: {
-                shared: true,
-                valueSuffix: ' millions'
-            },
-            plotOptions: {
-                area: {
-                    stacking: 'normal',
-                    lineColor: '#666666',
-                    lineWidth: 1,
-                    marker: {
-                        lineWidth: 1,
-                        lineColor: '#666666'
-                    }
-                }
-            },
-            series: [{
-                name: 'Asia',
-                data: [502, 635, 809, 947, 1402, 3634, 5268]
-            }, {
-                name: 'Africa',
-                data: [106, 107, 111, 133, 221, 767, 1766]
-            }, {
-                name: 'Europe',
-                data: [163, 203, 276, 408, 547, 729, 628]
-            }, {
-                name: 'America',
-                data: [18, 31, 54, 156, 339, 818, 1201]
-            }, {
-                name: 'Oceania',
-                data: [2, 2, 2, 6, 13, 30, 46]
-            }]
-        });
+	createPercentageCompleteChart('#tre-la-la-percent-complete', 85);
+	createCfdChart('#tre-la-la-cfd');
 });
